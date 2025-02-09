e9fd4b0128756a3bfda6c6a7b46716871d0d31f0
natnaeleyuel <natnaeleyuel77@gmail.com>
2025-02-09 22:28:48 +0300

    commit for route and db connect

--- EduReach-Backend/configuration/database.js
+++ EduReach-Backend/configuration/database.js
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose';
 const connectDB = async () => {
     try{
-        const conn = await mongoose.connect(process.env.DATABASE_URI);
+        const conn = await mongoose.connect(process.env.DATABASE_URI, {});
         console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
         console.log(`Error: ${error.message}`);
         process.exit(1)
     }
 }
+export default connectDB;
